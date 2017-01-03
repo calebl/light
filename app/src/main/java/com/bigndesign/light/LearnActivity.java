f0b0ecb38a6a263b1b62d9177f11c4f1d8ca9077
Caleb L <caleb.lenoir@gmail.com>
2017-01-02 22:09:51 -0500

    login works with auth0 but requires email address to signup

--- app/src/main/java/com/bigndesign/light/LearnActivity.java
+++ app/src/main/java/com/bigndesign/light/LearnActivity.java
@@ -4,9 +4,9 @@ package com.bigndesign.light;
  *  ExpandableListView adapted from http://theopentutorials.com/tutorials/android/listview/android-expandable-list-view-example/
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
@@ -44,8 +44,8 @@ public class LearnActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginActivity);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);

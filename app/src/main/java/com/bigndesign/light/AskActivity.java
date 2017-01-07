e16e11fd427a1a88d8076546886e0b4181dcc1ff
ticklegr <ticklegr@mail.uc.edu>
2017-01-07 13:58:24 -0500

    Add Sinch code

--- app/src/main/java/com/bigndesign/light/AskActivity.java
+++ app/src/main/java/com/bigndesign/light/AskActivity.java
@@ -1,13 +1,24 @@
 package com.bigndesign.light;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class AskActivity extends AppCompatActivity {
+    private String recipientId;
+    private EditText messageBodyField;
+    private String messageBody;
+    private MessageService.MessageServiceInterface messageService;
+    private String currentUserId;
+    private ServiceConnection serviceConnection = new MyServiceConnection();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,38 @@ public class AskActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
+
+        startService(serviceIntent);
+
+        messageBodyField = (EditText) findViewById(R.id.messageBodyField);
+
+        //listen for a click on the send button
+        findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //send the message!
+            }
+        });
     }
 
+    //unbind the service when the activity is destroyed
+    @Override
+    public void onDestroy() {
+        unbindService(serviceConnection);
+        super.onDestroy();
+    }
+
+    private class MyServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            messageService = (MessageService.MessageServiceInterface) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            messageService = null;
+        }
+    }
 }

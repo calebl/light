19da75abba25e5a98b74eaf6fc45433e861f9b49
ticklegr <ticklegr@mail.uc.edu>
2017-01-07 19:49:55 -0500

    AskActivity now shows messenger; added private listener class

--- app/src/main/java/com/bigndesign/light/AskActivity.java
+++ app/src/main/java/com/bigndesign/light/AskActivity.java
@@ -5,12 +5,20 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.sinch.android.rtc.PushPair;
+import com.sinch.android.rtc.messaging.Message;
+import com.sinch.android.rtc.messaging.MessageClient;
+import com.sinch.android.rtc.messaging.MessageClientListener;
+import com.sinch.android.rtc.messaging.MessageDeliveryInfo;
+import com.sinch.android.rtc.messaging.MessageFailureInfo;
+
+import java.util.List;
 
 public class AskActivity extends AppCompatActivity {
     private String recipientId;
@@ -23,23 +31,15 @@ public class AskActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ask);
+        setContentView(R.layout.messaging);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
 
         startService(serviceIntent);
+
+        bindService(new Intent(this, MessageService.class), serviceConnection, BIND_AUTO_CREATE);
 
         messageBodyField = (EditText) findViewById(R.id.messageBodyField);
 
@@ -47,7 +47,15 @@ public class AskActivity extends AppCompatActivity {
         findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //send the message!
+                //send the message
+                messageBody = messageBodyField.getText().toString();
+                if (messageBody.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                messageService.sendMessage(recipientId, messageBody);
+                messageBodyField.setText("");
             }
         });
     }
@@ -59,6 +67,7 @@ public class AskActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+
     private class MyServiceConnection implements ServiceConnection {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
@@ -69,5 +78,38 @@ public class AskActivity extends AppCompatActivity {
         public void onServiceDisconnected(ComponentName componentName) {
             messageService = null;
         }
+    }
+
+    private class MyMessageClientListener implements MessageClientListener {
+
+        //Notify the user if their message failed to send
+        @Override
+        public void onMessageFailed(MessageClient client, Message message,
+                                    MessageFailureInfo failureInfo) {
+            Toast.makeText(AskActivity.this, "Message failed to send.", Toast.LENGTH_LONG).show();
+        }
+
+
+        @Override
+        public void onIncomingMessage(MessageClient client, Message message) {
+            //Display an incoming message
+        }
+
+        @Override
+        public void onMessageSent(MessageClient client, Message message, String recipientId) {
+            //Display the message that was just sent
+
+            //Later, I'll show you how to store the
+            //message in Parse, so you can retrieve and
+            //display them every time the conversation is opened
+        }
+
+        //Do you want to notify your user when the message is delivered?
+        @Override
+        public void onMessageDelivered(MessageClient client, MessageDeliveryInfo deliveryInfo) {}
+
+        //Don't worry about this right now
+        @Override
+        public void onShouldSendPushData(MessageClient client, Message message, List<PushPair> pushPairs) {}
     }
 }

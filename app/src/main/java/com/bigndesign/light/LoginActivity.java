0a36c12bd33d55b4211ee58ee122e62c30f01860
ticklegr <ticklegr@mail.uc.edu>
2017-02-16 21:59:30 -0500

    Added Toolbar to LoginActivity; removed second setContentView

--- app/src/main/java/com/bigndesign/light/LoginActivity.java
+++ app/src/main/java/com/bigndesign/light/LoginActivity.java
@@ -13,6 +13,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -46,10 +47,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         MeteorSingleton.getInstance().connect();
-
-        setContentView(R.layout.activity_login);
 
         // Set up the login form.
         mUsernameView = (EditText) findViewById(R.id.email);
